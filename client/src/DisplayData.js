ef697e0e838b9820f3c62946b6444e49d6608de2
“Dustin <dustin@inleague.org>
2023-01-14 12:10:58 -0600

    first commit

---
+++ client/src/DisplayData.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {useQuery, gql } from '@apollo/client'
+
+const QUERY_ALL_USERS = gql`
+    query GetAllUsers {
+        users {
+            id
+            name
+            age
+            username
+        }
+    }
+`
+export const DisplayData = () => {
+    const {data, loading, error} = useQuery(QUERY_ALL_USERS);
+    if(loading) {
+        return <h1>DATA IS LOADING...</h1>;
+    }
+    if(data) {
+        console.log(data)
+    }
+    if(error) {
+        console.log(error)
+    }
+    return <div>
+            {data && 
+                data.users.map((user) => {
+                    return <div> 
+                                <h1>Name {user.name} </h1> 
+                                <h1>Username: {user.username} </h1> 
+                                <h1> Age: {user.age} </h1> 
+                            </div>
+                })}
+        </div>
+}
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const {ApolloServer} =  require('apollo-server');
+const {typeDefs} = require('./schema/type-defs');
+const {resolvers} = require('./schema/resolvers');
+
+
+//apollo server is the most popular graphql server library
+//every piece of data exists inside typeDefs
+//every function in graphql is enclosed inside resolvers
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.listen().then((url) => {
+    console.log(`YOUR API IS RUNNING AT ${url}`)
+});
